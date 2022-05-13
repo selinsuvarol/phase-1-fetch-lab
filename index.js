155b8e53ffb2f392fa0bc95e85b782ad7f8963c4
Selin Su Varol <selinsuvarol@gmail.com>
2022-05-13 14:05:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(json => renderBooks(json));
 }
 
-function renderBooks(books) {
+function renderBooks(json) {
   const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  json.forEach(jsonData => {
+    const h4 = document.createElement('h4');
+    h4.innerHTML =jsonData.name;
+    main.appendChild(h4);
   });
 }
 
